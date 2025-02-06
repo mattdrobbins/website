859e423963b2240172c65ed3359c93952355b3c6
Matthew Robbins <mattdrobbins@protonmail.com>
2025-02-06 16:17:33 -0500

    updates

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-set -e
-set -x
+npx @tailwindcss/cli -i ./wwwroot/input.css -o ./wwwroot/output.css
 
 rm -f middleware.exe
 dune build
---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo ./middleware.exe & npx @tailwindcss/cli -i ./wwwroot/input.css -o ./wwwroot/output.css --watch
