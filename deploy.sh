04c528a271796ef4ad4f73e126c7b8808cc3ae3d
Matthew Robbins <mattdrobbins@protonmail.com>
2025-02-06 16:00:03 -0500

    Add tailwind

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-cp /home/build/app/app.service /etc/systemd/system/
+cp /root/app/app.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl restart app
--- middleware.ml
+++ middleware.ml
@@ -1,5 +1,5 @@
 let () =
-  Dream.run ~interface:"0.0.0.0" ~port:80
-  @@ Dream.logger  @@ Dream.router [
-    Dream.get "/" (fun _ -> Dream.html "Dream started by systemd!");
-  ]
+  Dream.run ~interface:"0.0.0.0" ~port:443 ~tls:true
+  @@ Dream.logger @@ Dream.router [
+    (Dream.get "/" (Dream.from_filesystem "wwwroot" "index.html"));
+    (Dream.get "/**" (Dream.static "./wwwroot"))];
